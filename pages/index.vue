d8c67db823c509be4b1ccc70771f40f52a920bba
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-10-30 16:52:21 +0100

    Make report totally work

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">reportaplayer</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+    <div v-if="!reported" class="grid grid-cols-1">
+      <h1 class="title">WHO DO YOU WANT TO REPORT?</h1>
+      <div class="grid grid-cols-2">
+        <input
+          v-model="currentReportee"
+          class="text-3xl border-solid border-2 border-gray-400 p-5"
+          type="text"
+          @keydown.enter="report()"
+        />
+        <button
+          class="border-solid border-2 border-red-900"
+          type="submit"
+          @click="report()"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+          REPORT PLAYER!
+        </button>
       </div>
+    </div>
+    <div v-else>
+      <h1 class="text-6xl">Reported!</h1>
+      <h2 class="text-4xl">
+        All of our agents are now taking immediate action against <br />
+        <strong>{{ reportee }}</strong>
+      </h2>
+      <p class="">
+        The player "{{ reportee }}" will receive the most appropriate justice
+        that you deem necessary in your own mind.
+      </p>
+      <button
+        class="my-20 border-solid border-2 border-brown-500 p-5 text-3xl"
+        type="button"
+        @click="reset()"
+      >
+        I am satisfied and I want to report another!
+      </button>
     </div>
   </div>
 </template>
@@ -28,22 +42,31 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      reported: false,
+      currentReportee: '',
+      reportee: '',
+    }
+  },
+  methods: {
+    reset() {
+      this.reported = false
+    },
+    report() {
+      if (!this.currentReportee) return
+      this.reportee = this.currentReportee
+      this.currentReportee = ''
+      this.reported = true
+    },
+  },
+})
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
+<style lang="postcss">
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 
 .title {
@@ -51,20 +74,8 @@ export default Vue.extend({})
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 60px;
   color: #35495e;
   letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
